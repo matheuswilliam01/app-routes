9ea1aa2835bd3ea6dd18455774fb52b88fd6e284
Matheus William <matheuswilliam2002@hotmail.com>
2022-08-09 16:08:38 -0300

    Login guard

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
+  // Bloquear/Permitir o acesso as rotas privadas.
+  private router: Router;
+
+  constructor(router: Router) {
+    this.router = router;
+  }
+  
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    
+    if(UserService.authUser()) { //verifica se o usuário existe
+      return true;
+    }
+    else{
+      alert("Usuário e/ou senha inválido!")
+      this.router.navigate(["/login"]); // joga pra tela de login
+      return false;
+    }
   }
-  
 }
